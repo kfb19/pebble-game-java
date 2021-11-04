0f96bcc6fbe71084f20d07886d315232e1bc1a57
MichaelHills01 <75442155+MichaelHills01@users.noreply.github.com>
2021-11-04 14:25:23 +0000

    Updating working code

--- Main.java
+++ Main.java
@@ -1,2 +1,37 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+
 public class Main {
+    public static void main(String[] args) {
+
+        PebbleGame pebbleGame = new PebbleGame();
+        pebbleGame.setNoUsers(pebbleGame.printMenu());
+
+        File black0 = pebbleGame.getFile(0);
+        File black1 = pebbleGame.getFile(1);
+        File black2 = pebbleGame.getFile(2);
+
+        BlackBag blackBag1 = null;
+        BlackBag blackBag2 = null;
+        BlackBag blackBag3 = null;
+
+        try {
+            blackBag1 = new BlackBag(black0);
+            blackBag2 = new BlackBag(black1);
+            blackBag3 = new BlackBag(black2);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        pebbleGame.setUsers();
+
+        
+
+
+
+
+
+
+    }
 }
